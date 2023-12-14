700848a17033a3c3bf2e179775dd53c01810fcdd
Akirachii <42194.5a5@gmail.com>
2023-12-14 15:52:03 +0100

    added exercise

---
+++ UD6/Organigrama.java
@@ -0,0 +1,14 @@
+public class Organigrama{
+    public static int organigram(int N) {
+        
+        return res;
+    }
+
+    
+    public static void main (String[] args){
+        System.out.println(organigram(Integer.valueOf(args[0])));
+        System.out.println(organigram(Integer.valueOf(args[1])));
+        System.out.println(organigram(Integer.valueOf(args[2])));
+        System.out.println(organigram(Integer.valueOf(args[3])));
+    }
+}
