6df5a5c947ac2487b9fa35e1ec2faed363590cd7
Akirachii <42194.5a5@gmail.com>
2023-12-14 15:15:13 +0100

    Added new exercise

---
+++ EjFibonacci.java
@@ -0,0 +1,17 @@
+public class EjFibonacci{
+    public static int fibonacci(int N) {
+        int res;
+        if (N == 0) {
+        res = 0;
+        } else if (N == 1) {
+            res=1;
+        } else {
+            res = fibonacci(N - 1) + fibonacci(N - 2);
+        }
+        return res;
+    }
+    public static void main (String[] args){
+        System.out.println(fibonacci(Integer.valueOf(args[0])));
+        System.out.println(fibonacci(Integer.valueOf(args[1])));
+    }
+}
