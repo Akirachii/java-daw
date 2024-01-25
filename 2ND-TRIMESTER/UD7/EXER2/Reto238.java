0283a83fadb34ac12cb73bf55e81c65d85192a57
Akirachii <42194.5a5@gmail.com>
2024-01-25 19:13:55 +0100

    Added missing files

---
+++ 2ND-TRIMESTER/UD7/EXER2/Reto238.java
@@ -0,0 +1,9 @@
+
+public class Reto238 {
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ 2ND-TRIMESTER/UD7/EXER2/Reto702.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Reto702 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter values: ");
+        String val=sc.nextLine();
+        
+        for (int x=0;x.equals(sepval[1]);x++){
+
+
+        }
+
+
+    }
+
+
+}
