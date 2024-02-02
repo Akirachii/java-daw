0ae04eb6f1507273179ea04d2429f533b91b6ec2
Akirachii <42194.5a5@gmail.com>
2024-02-02 16:47:02 +0100

    Added lib and pizza

---
+++ 2ND-TRIMESTER/UD8/lib/Pizza.java
@@ -0,0 +1,42 @@
+package lib;
+public class Pizza {
+
+    private String tamanio;
+    private String tipo;
+    private String estado;  
+    private static int TotalPedidas;
+    private static int TotalServidas;
+
+    public Pizza(String tipo ,String tamanio){
+        this.tipo = tipo;
+        this.tamanio = tamanio;
+        this.estado = "pedida";
+        TotalPedidas += 1;
+    }
+    public void sirve(){
+        if ("servida".equals(this.estado) ){
+            System.out.println("esa pizza ya se ha servido");
+
+        }else {
+            this.estado = "servida";
+            TotalServidas += 1;
+        }
+    }
+
+    //geters
+    public String getTamanio(){
+        return this.tamanio;
+    }
+    public static int getTotalServidas(){
+        return TotalServidas;
+    }
+    public static int getTotalPedidas(){
+        return TotalPedidas;
+    }
+
+    public String toString(){
+        return "pizza " + tipo + " , " + tamanio + " ," + estado;
+    }
+
+ 
+}
